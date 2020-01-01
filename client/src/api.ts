5eec567098cea79e823d88a2370862b5189aad83
stpoa <stpoaa+dev@gmail.com>
2020-01-01 20:35:54 +0100

    Add post and put to api

--- client/src/api.ts
+++ client/src/api.ts
@@ -4,8 +4,18 @@ import { getConfig } from './env'
 const api = axios.create({ baseURL: getConfig().apiUrl })
 
 type Get = <T = any>(url: string, config?: AxiosRequestConfig) => Promise<T>
+type Post = <T = any>(url: string, config?: AxiosRequestConfig) => Promise<T>
+type Put = <T = any>(url: string, config?: AxiosRequestConfig) => Promise<T>
+
 export const get: Get = (url, config) =>
   api.get(url, config).then(response => response.data)
+
+export const post: Post = (url, config) =>
+  api.post(url, config).then(response => response.data)
+
+export const put: Put = (url, config) =>
+  api.put(url, config).then(response => response.data)
+
 
 export function wrapPromise<T>(promise: Promise<T>) {
   let status = 'pending'
