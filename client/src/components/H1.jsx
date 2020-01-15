1e3c3e0895f280366423067dd9768ecb4616ca11
stpoa <stpoaa+dev@gmail.com>
2020-01-16 00:39:30 +0100

    Change header font size

--- client/src/components/H1.jsx
+++ client/src/components/H1.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Typography } from '@material-ui/core'
 
 export const H1 = ({ children }) => (
-  <Typography component="h1" variant="h3">
+  <Typography component="h1" variant="h4">
     {children}
   </Typography>
 )
