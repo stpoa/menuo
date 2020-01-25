68833e9bcfd748dac3da2a4fb940fa9bae1ab765
stpoa <stpoaa+dev@gmail.com>
2020-01-25 13:11:21 +0100

    Fix false positive logins with wrong password

--- server/src/auth/validation.ts
+++ server/src/auth/validation.ts
@@ -1,7 +1,4 @@
 import bcrypt from 'bcryptjs'
 
 export const isPasswordValid = (eventPassword: string, userPassword: string) =>
-  bcrypt
-    .compare(eventPassword, userPassword)
-    .then(() => true)
-    .catch(() => false)
+  bcrypt.compare(eventPassword, userPassword)
