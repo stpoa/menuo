9c018f0a83fbe38e85310a03c44dedcafc03c30e
stpoa <stpoaa+dev@gmail.com>
2020-02-02 12:07:47 +0100

    Fix typo

--- packages/server/scripts/start-local.js
+++ packages/server/scripts/start-local.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const { spawn } = require('child_process')
 const { LOCAL_PORT } = process.env
 
-console.log('Starting local server op port: ' + LOCAL_PORT)
+console.log('Starting local server on port: ' + LOCAL_PORT)
 
 spawn('sls', ['offline', 'start', '--port', LOCAL_PORT], {
   stdio: 'inherit',
