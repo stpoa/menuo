0824b9e51e7cc4e30ff03a5fdf5fabd7a4df108e
stpoa <stpoaa+dev@gmail.com>
2020-02-04 22:17:12 +0100

    Rename actions

--- packages/client/src/app-customer/store/basket/actions.ts
+++ packages/client/src/app-customer/store/basket/actions.ts
@@ -1,25 +1,25 @@
 import { createAction } from '@reduxjs/toolkit'
 
-export const basketAdd = createAction('BASKET_ADD', basketEntry => ({
+export const basketAdd = createAction('BASKET / ADD', basketEntry => ({
   payload: { basketEntry },
 }))
 
-export const basketRemove = createAction('BASKET_REMOVE', basketEntry => ({
+export const basketRemove = createAction('BASKET / REMOVE', basketEntry => ({
   payload: { basketEntry },
 }))
 
 export const basketToggleVariant = createAction(
-  'BASKET_TOGGLE_VARIANT',
+  'BASKET / TOGGLE_VARIANT',
   basketEntry => ({
     payload: { basketEntry },
   }),
 )
 
 export const basketToggleDish = createAction(
-  'BASKET_TOGGLE_DISH',
+  'BASKET / TOGGLE_DISH',
   basketEntry => ({
     payload: { basketEntry },
   }),
 )
 
-export const basketClear = createAction('BASKET_CLEAR')
+export const basketClear = createAction('BASKET / CLEAR')
