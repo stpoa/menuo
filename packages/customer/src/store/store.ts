fc5d6978f7b6f73efbe9c44397e966f9302e5a7c
stpoa <stpoaa+dev@gmail.com>
2020-02-09 13:13:39 +0100

    Save state and restore from localstorage

--- packages/customer/src/store/store.ts
+++ packages/customer/src/store/store.ts
@@ -1,4 +1,6 @@
 import { createStore, applyMiddleware } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import { createHashHistory } from 'history'
 import { createEpicMiddleware } from 'redux-observable'
 import { composeWithDevTools } from 'redux-devtools-extension'
@@ -11,12 +13,19 @@ import { rootEpic } from './epic'
 export const history = createHashHistory()
 const epicMiddleware = createEpicMiddleware<any>()
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
 export const store = createStore(
-  createRootReducer(history),
+  persistReducer(persistConfig, createRootReducer(history)),
   composeWithDevTools(
     applyMiddleware(epicMiddleware, routerMiddleware(history)),
   ),
 )
+
+export const persistor = persistStore(store)
 
 epicMiddleware.run(rootEpic)
 
