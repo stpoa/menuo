a5a3ccefb92fbf33c54bc7935b9eb99332bbcc61
stpoa <stpoaa+dev@gmail.com>
2020-02-09 13:51:57 +0100

    Generate qr code in terminal

---
+++ packages/customer/scripts/generate-qr.js
@@ -0,0 +1,9 @@
+const qrcode = require('qrcode-terminal')
+const ip = require('ip')
+
+const hostIp = ip.address()
+const localhostUrl = 'http://' + hostIp + ':' + process.env.PORT
+
+const urlBase = process.env.PUBLIC_URL || localhostUrl
+
+qrcode.generate(urlBase + '/#/kolanko/?table=1')
