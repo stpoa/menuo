3b3481a75aa8230da4f588988e88504cdae03e3f
stpoa <stpoaa+dev@gmail.com>
2020-02-18 20:51:10 +0100

    Remove dist from qr

--- packages/qr/dist/qrcode-monkey.d.ts
+++
@@ -1,6 +0,0 @@
-export interface QRPathData {
-    path: string;
-    data: string;
-}
-export declare const getFolderPath: (path: string) => string;
-export declare const createQrCode: (parameters?: {}) => ({ path, data, }: QRPathData) => Promise<void>;
