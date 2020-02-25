9bfcd3296d82a53dd2752e5f9883552c14544f30
stpoa <stpoaa+dev@gmail.com>
2020-02-25 11:39:36 +0100

    Refactor languages

--- packages/customer/src/store/user/locale/api.ts
+++ packages/customer/src/store/user/locale/api.ts
@@ -6,3 +6,32 @@ export const getLanguageFromBrowser = () => {
 
   return language.toLowerCase().split(/[_-]+/)[0]
 }
+
+export const getTranslations = () => ({
+  callWaiter: ['Call a waiter', 'Zawołaj kelnera'],
+  order: ['Order', 'Zamów'],
+  searchPlaceholderContent: [
+    'Search in dishes and descriptions',
+    'Wyszukaj w daniach lub w opisach',
+  ],
+  homePageUrlContent: ['https://menuo.app/en', 'https://menuo.app'],
+  aboutUs: ['About us', 'O nas'],
+  payByCash: ['Pay by cash', 'Płatność gotówką'],
+  payByCard: ['Pay by card', 'Płatność kartą'],
+  other: ['Other', 'Inne'],
+  orderConfirmContent: [
+    'Do you want to order entries below?',
+    'Czy chcesz zamówić poniższe?',
+  ],
+  back: ['back', 'powrót'],
+  orderSentTitleContent: ['Order sent', 'Zamówienie wysłane!'],
+  orderSentSubTitleContent: [
+    'Your order has been sent',
+    'Twoje zamówienie zostało wysłane!',
+  ],
+  orderSentContent: [
+    'Remember that at any moment you can make another order or call a waiter. Already ordered dishes can be seen by clicking an icon in upper right screen corner.',
+    'Pamiętaj, że w każdej chwili możesz złożyć kolejne zamówienie lub zawołać kelnera. Zamówione wcześniej dania możesz sprawdzić klikając ikonke w prawym, górnym rogu.',
+  ],
+  currentlyInBasket: ['Currently in basket', 'Aktualnie w koszyku'],
+})
--- packages/customer/src/store/user/locale/epic.ts
+++ packages/customer/src/store/user/locale/epic.ts
@@ -1,11 +1,11 @@
 import { Epic } from 'redux-observable'
-import { filter, mergeMap, map, skip } from 'rxjs/operators'
+import { filter, mergeMap, skip } from 'rxjs/operators'
 import { isOfType, isActionOf } from 'typesafe-actions'
 import { initialize, setActiveLanguage } from 'react-localize-redux'
 import { renderToStaticMarkup } from 'react-dom/server'
 
 import * as actions from './actions'
-import { getLanguageFromBrowser } from './api'
+import { getLanguageFromBrowser, getTranslations } from './api'
 
 import * as allActions from '../../actions'
 
@@ -20,43 +20,15 @@ export const localeInitEpic: Epic = action$ =>
   action$.pipe(
     filter(isActionOf(actions.localeInit)),
     mergeMap(() => {
-      const language = getLanguageFromBrowser()
-      const activeLanguage = language === 'pl' ? 'pl' : 'en'
-
-      const translation = {
-        callWaiter: ['Call a waiter', 'Zawołaj kelnera'],
-        order: ['Order', 'Zamów'],
-        searchPlaceholderContent: [
-          'Search in dishes and descriptions',
-          'Wyszukaj w daniach lub w opisach',
-        ],
-        homePageUrlContent: ['https://menuo.app/en', 'https://menuo.app'],
-        aboutUs: ['About us', 'O nas'],
-        payByCash: ['Pay by cash', 'Płatność gotówką'],
-        payByCard: ['Pay by card', 'Płatność kartą'],
-        other: ['Other', 'Inne'],
-        orderConfirmContent: [
-          'Do you want to order entries below?',
-          'Czy chcesz zamówić poniższe?',
-        ],
-        back: ['back', 'powrót'],
-        orderSentTitleContent: ['Order sent', 'Zamówienie wysłane!'],
-        orderSentSubTitleContent: [
-          'Your order has been sent',
-          'Twoje zamówienie zostało wysłane!',
-        ],
-        orderSentContent: [
-          'Remember that at any moment you can make another order or call a waiter. Already ordered dishes can be seen by clicking an icon in upper right screen corner.',
-          'Pamiętaj, że w każdej chwili możesz złożyć kolejne zamówienie lub zawołać kelnera. Zamówione wcześniej dania możesz sprawdzić klikając ikonke w prawym, górnym rogu.',
-        ],
-        currentlyInBasket: ['Currently in basket', 'Aktualnie w koszyku'],
-      }
+      const languages = ['en', 'pl']
+      const defaultLanguage = 'en'
+      const activeLanguage = getLanguageFromBrowser() === 'pl' ? 'pl' : 'en'
 
       return [
         initialize({
-          languages: ['en', 'pl'],
-          translation,
-          options: { defaultLanguage: 'en', renderToStaticMarkup },
+          languages,
+          translation: getTranslations(),
+          options: { defaultLanguage, renderToStaticMarkup },
         }),
         setActiveLanguage(activeLanguage),
       ]
