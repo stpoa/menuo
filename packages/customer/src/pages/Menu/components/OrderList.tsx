1c91afb2f8744b39fb47e4cf161337cfc331cae7
stpoa <stpoaa+dev@gmail.com>
2020-05-02 09:38:04 +0200

    Add price sum calculation

--- packages/customer/src/pages/Menu/components/OrderList.tsx
+++ packages/customer/src/pages/Menu/components/OrderList.tsx
@@ -9,35 +9,53 @@ import {
   createStyles,
   withStyles,
   WithStyles,
+  Divider,
 } from '@material-ui/core'
+import { add } from 'ramda'
 
 interface OrderedListProps extends WithStyles {
   ordered: [MenuEntry, number][]
 }
 
 export const OrderedList: FC<OrderedListProps> = ({ ordered, classes }) => (
-  <div>
-    <List>
-      {ordered.map(([entry, count]) => (
-        <ListItem dense disableGutters key={entry.dishName}>
-          <ListItemIcon className={classes.itemIcon}>
-            <span>{count} x</span>
-          </ListItemIcon>
+  <List>
+    {ordered.map(([entry, count]) => (
+      <ListItem dense disableGutters key={entry.dishName}>
+        <ListItemIcon className={classes.itemIcon}>
+          <span>{count} x</span>
+        </ListItemIcon>
 
-          <ListItemText className={classes.itemText}>
-            {truncate(80)(entry.dishName)}
-            {entry.dishVariantName
-              ? ' - ' + truncate(80)(entry.dishVariantName)
-              : ''}
-          </ListItemText>
+        <ListItemText className={classes.itemText}>
+          {truncate(80)(entry.dishName)}
+          {entry.dishVariantName
+            ? ' - ' + truncate(80)(entry.dishVariantName)
+            : ''}
+        </ListItemText>
 
-          <ListItemIcon className={classes.itemIcon}>
-            <span>{count * entry.dishVariantPrice} zł</span>
-          </ListItemIcon>
-        </ListItem>
-      ))}
-    </List>
-  </div>
+        <ListItemIcon className={classes.itemIcon}>
+          <span>{count * entry.dishVariantPrice} zł</span>
+        </ListItemIcon>
+      </ListItem>
+    ))}
+
+    <Divider />
+    <ListItem dense disableGutters key="sum">
+      <ListItemIcon className={classes.itemIcon}>
+        <span>Σ</span>
+      </ListItemIcon>
+
+      <ListItemText className={classes.itemText}></ListItemText>
+
+      <ListItemIcon className={classes.itemIcon}>
+        <span>
+          {ordered
+            .map(([entry, count]) => entry.dishVariantPrice * count)
+            .reduce(add, 0)}{' '}
+          zł
+        </span>
+      </ListItemIcon>
+    </ListItem>
+  </List>
 )
 
 const styleComponent = withStyles(() =>
