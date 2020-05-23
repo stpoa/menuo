89b7ec87fd8e6b0455dfd60b3e5cbbc996a74b25
marcindyjas <marcin.dyjas@gmail.com>
2020-05-23 17:48:30 +0200

    Add script - create links from jsn woth table id

---
+++ packages/qr/scripts/tables-to-json-restaurant.py
@@ -0,0 +1,11 @@
+import json
+
+with open('../data/restaurant.json', 'r') as f:
+    tables = json.load(f)
+
+links = open('create-qr-code-restaurant.ts', 'w')
+links.write('import { createQrCode } from \'../src/qrcode-monkey\'\n;[\n')
+for i in tables:
+    links.write('  { data: \'https://menuo.app/menu/#/demo?table=' + i + '\', path: \'' + i + '.png\' },\n')
+links.write('].map(createQrCode())\n')
+links.close()
