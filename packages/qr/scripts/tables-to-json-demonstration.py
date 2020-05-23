55865f9caf297d77be2084d0c5ad6ee0a245169d
marcindyjas <marcin.dyjas@gmail.com>
2020-05-23 19:12:08 +0200

    Edit path in scripts

--- packages/qr/scripts/tables-to-json-demonstration.py
+++ packages/qr/scripts/tables-to-json-demonstration.py
@@ -1,9 +1,9 @@
 import json
 
-with open('../data/demonstration.json', 'r') as f:
+with open('./data/demonstration.json', 'r') as f:
     tables = json.load(f)
 
-links = open('create-qr-code-demonstration.ts', 'w')
+links = open('./scripts/create-qr-code-demonstration.ts', 'w')
 links.write('import { createQrCode } from \'../src/qrcode-monkey\'\n;[\n')
 for i in tables:
     links.write('  { data: \'https://menuo.app/menu/#/demo?table=1&qr=1' + i + '\', path: \'1' + i + '.png\' },\n')
--- packages/qr/scripts/tables-to-json-restaurant.py
+++ packages/qr/scripts/tables-to-json-restaurant.py
@@ -1,9 +1,9 @@
 import json
 
-with open('../data/restaurant.json', 'r') as f:
+with open('./data/restaurant.json', 'r') as f:
     tables = json.load(f)
 
-links = open('create-qr-code-restaurant.ts', 'w')
+links = open('./scripts/create-qr-code-restaurant.ts', 'w')
 links.write('import { createQrCode } from \'../src/qrcode-monkey\'\n;[\n')
 for i in tables:
     links.write('  { data: \'https://menuo.app/menu/#/demo?table=' + i + '\', path: \'' + i + '.png\' },\n')
